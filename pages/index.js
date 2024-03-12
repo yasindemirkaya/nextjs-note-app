b5fbb3c4fe7c1010383f0c1ed1cc424fc6477fe0
yasindemirkaya <yasin97demirkaya@gmail.com>
2024-03-12 22:56:17 +0300

    Main
    Mongodb integration

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,33 @@
-import { useSelector } from 'react-redux';
 import NoteGrid from '../components/notes/note-grid'
+import { connectToDatabase } from '@/utils/db'
 
-function Home() {
-  const notes = useSelector(state => state.notes.notes)
-
+function Home(props) {
   return (
     <div className='container'>
       <h1 className='my-2'>My Notes</h1>
-      <NoteGrid notes={notes} />
+      <NoteGrid notes={props.notes} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection('notes');
+    console.log('Collection: ', collection)
+    const data = await collection.find({}).toArray();
+    console.log('Data: ', data)
+    return {
+      props: {
+        notes: data
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return {
+      props: { data: [] },
+    };
+  }
 }
 
 export default Home;
